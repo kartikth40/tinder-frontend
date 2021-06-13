a8dafd8f1f19ff26df946a266d119714206b7d18
Kartik <kartikth40@gmail.com>
2021-06-13 09:27:52 +0530

    improved loading animations

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,16 +11,32 @@ const App = () => {
   const [cards, setCards] = useState([])
 
   useEffect(() => {
+    const loader = document.querySelector('.loader-container')
     async function fetchData() {
       const req = await axios.get('/tinder/cards')
       setCards(req.data)
-      document.querySelector('.loader-container').classList.add('fade-out')
-      setInterval(() => {
-        document.querySelector('.loader-container').classList.add('hide')
-      }, 1000)
+      let time = 500
+      if (sessionStorage.getItem('firstTime') === 'true') time = 1500
+      console.log(time)
+      runAfter(time, () => loader.classList.add('fade-out'))
+      runAfter(time + 500, () => loader.classList.add('hide'))
     }
+    function isFirstTime() {
+      if (sessionStorage.getItem('firstTime') === 'true') {
+        sessionStorage.setItem('firstTime', 'false')
+      } else if (sessionStorage.getItem('firstTime') === null) {
+        sessionStorage.setItem('firstTime', 'true')
+      }
+    }
+    isFirstTime()
     fetchData()
   }, [])
+
+  const runAfter = (time, func) => {
+    setInterval(() => {
+      func()
+    }, time)
+  }
 
   return (
     <div className="container">
